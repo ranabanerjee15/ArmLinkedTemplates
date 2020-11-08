43bd3f11a6bdf6dc85bda8698758577604a579a9
Rana Banerjee <rana.b4523@gmail.com>
2020-11-09 09:00:57 +1000

    changes

--- artifacts/Deploy.ps1
+++ artifacts/Deploy.ps1
@@ -8,6 +8,6 @@ New-AzResourceGroupDeployment -WhatIf -Name "HubVnetDeploy" -ResourceGroupName $
 New-AzResourceGroupDeployment -Name "HubVnetDeploy" -ResourceGroupName $rg -TemplateFile .\main.json -Verbose -Mode Complete
 
 # Test against best practices
-Import-Module C:\Users\ranab\OneDrive\Documents\ArmLinkedTemplates\arm-template-toolkit\arm-ttk\arm-ttk.psm1
+Import-Module C:\Users\ranab\OneDrive\Documents\ArmLinkedTemplates\arm-template-toolkit\arm-ttk\arm-ttk.psd1
 Import-Module .\arm-template-toolkit\arm-ttk\arm-ttk.psm1 -Verbose
 Test-AzTemplate -Templatepath .\main.json
